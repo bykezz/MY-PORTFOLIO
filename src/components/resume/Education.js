51b14f51deb42a4a16c8b6c12b8efb1bce4f4000
bykezz <chibuikepascal31@gmail.com>
2024-04-03 14:11:32 +0100

    me

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -13,7 +13,7 @@ const Education = () => {
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2008 - 2020</p>
-          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
+          <h2 className="text-3xl md:text-4xl font-bold">Education </h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full  border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
