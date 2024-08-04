cfb00b6b70ae821502def0ecea8f59ebd921b8c2
bykezz <chibuikepascal31@gmail.com>
2024-08-04 13:22:35 +0100

    me

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -89,7 +89,7 @@ const Skills = () => {
           <p className="text-sm text-designColor tracking-[4px] uppercase">
             Features
           </p>
-          <h2 className="text-3xl md:text-4xl font-bold">Development Skill</h2>
+          <h2 className="text-3xl md:text-4xl font-bold">Development Skills</h2>
         </div>
         <div className="flex flex-col gap-6">
           <div className="overflow-x-hidden">
